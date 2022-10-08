824d11e819c7294e6cb317fc40cd3de194729995
M Sreekanth <m.sreekanth112@gmail.com>
2022-10-08 13:14:40 +0530

    RemoteLoader request data on load

--- GitHubTests/RemotePullReqLoaderTests.swift
+++ GitHubTests/RemotePullReqLoaderTests.swift
@@ -8,20 +8,43 @@
 import XCTest
 
 class RemotePullRequestLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://api.github.com/repos/apple/swift/pulls?page=1&per_page=10")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class PullReqLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemotePullRequestLoader()
+        let client = HTTPClientSpy()
+        _ = RemotePullRequestLoader(client: client)
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemotePullRequestLoader(client: client)
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
