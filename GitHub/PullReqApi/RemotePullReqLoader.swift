e1d9bd11874ec4688606afcb10e221113414ad1e
M Sreekanth <m.sreekanth112@gmail.com>
2022-10-08 16:20:27 +0530

    Combine Url and competitions into tuple

--- GitHub/PullReqApi/RemotePullReqLoader.swift
+++ GitHub/PullReqApi/RemotePullReqLoader.swift
@@ -11,16 +11,22 @@ final class RemotePullRequestLoader {
     private let client: HTTPClient
     private let url: URL
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
     
-    func load() {
-        client.get(from: url)
+    func load(completion: @escaping (Error) -> Void) {
+        client.get(from: url, completion: { error in
+            completion(.connectivity)
+        })
     }
 }
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
--- GitHubTests/RemotePullReqLoaderTests.swift
+++ GitHubTests/RemotePullReqLoaderTests.swift
@@ -11,27 +11,35 @@ import XCTest
 class PullReqLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://api.github.com/repos/apple/swift/pulls?page=1&per_page=10")!
-        let client = HTTPClientSpy()
-        _ = RemotePullRequestLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://api.github.com/repos/apple/swift/pulls?page=1&per_page=10")!
-        let client = HTTPClientSpy()
-        let sut = RemotePullRequestLoader(url: url, client: client)
-        sut.load()
+        let (sut, client) = makeSUT(url: url)
+        sut.load {_ in }
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_load_requestsDataFromURLTwice() {
         let url = URL(string: "https://api.github.com/repos/apple/swift/pulls?page=1&per_page=10")!
-        let client = HTTPClientSpy()
-        let sut = RemotePullRequestLoader(url: url, client: client)
-        sut.load()
-        sut.load()
+        let (sut, client) = makeSUT(url: url)
+        sut.load {_ in }
+        sut.load {_ in }
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(string: "https://api.github.com/repos/apple/swift/pulls?page=1&per_page=10")!
+        let (sut, client) = makeSUT(url: url)
+        var captureError = [RemotePullRequestLoader.Error]()
+        sut.load {
+            captureError.append($0)
+        }
+        let error = NSError(domain: "Test", code: 0)
+        client.complete(with: error, at: 0)
+        XCTAssertEqual(captureError, [.connectivity])
     }
     
     // MARK: Helpers
@@ -39,15 +47,20 @@ class PullReqLoaderTests: XCTestCase {
     private func makeSUT(url: URL = URL(string: "https://api.github.com")!) -> (sut: RemotePullRequestLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemotePullRequestLoader(url: url, client: client)
-        sut.load()
         return (sut: sut, client: client)
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
+        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            messages.append((url, completion))
+        }
         
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(error)
         }
     }
 }
