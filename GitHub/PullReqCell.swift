12230e56959fc0471c72adfd271bb758a9e8178b
M Sreekanth <m.sreekanth112@gmail.com>
2022-10-09 13:46:16 +0530

    Design cell

--- GitHub/PullReqCell.swift
+++ GitHub/PullReqCell.swift
@@ -19,7 +19,7 @@ class PullReqCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
