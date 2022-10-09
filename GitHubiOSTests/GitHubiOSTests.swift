e238cb7c91fa15dc3eacf950ba90137fe75c2ff0
M Sreekanth <m.sreekanth112@gmail.com>
2022-10-09 14:43:13 +0530

    Pull to refresh the data

--- GitHubiOSTests/GitHubiOSTests.swift
+++ GitHubiOSTests/GitHubiOSTests.swift
@@ -7,17 +7,24 @@
 
 import XCTest
 import UIKit
+@testable import GitHub
 
-final class PullsViewController: UIViewController {
-    let loader: PullsViewControllerTests.LoaderSpy
-    init(loader: PullsViewControllerTests.LoaderSpy) {
+final class PullsViewController: UITableViewController {
+    let loader: PullRequestsLoader
+    init(loader: PullRequestsLoader) {
         self.loader = loader
         super.init(nibName: nil, bundle: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loader.load()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refreshAction), for: .valueChanged)
+        loader.load() { _ in }
+    }
+    
+    @objc func refreshAction() {
+        loader.load() { _ in }
     }
     
     required init?(coder: NSCoder) {
@@ -30,25 +37,60 @@ final class PullsViewController: UIViewController {
 final class PullsViewControllerTests: XCTestCase {
     
     func test_init_doesNotLoadPullsOnInitialisation() {
-        let spy = LoaderSpy()
-        _ = PullsViewController(loader: spy)
-        XCTAssertEqual(spy.callCount, 0)
+        let (_, loader) = makeSUT()
+        XCTAssertEqual(loader.callCount, 0)
     }
     
     func test_viewDidLoad_loadPulls() {
-        let spy = LoaderSpy()
-        let sut = PullsViewController(loader: spy)
+        let (sut, loader) = makeSUT()
         sut.loadViewIfNeeded()
-        XCTAssertEqual(spy.callCount, 1)
+        XCTAssertEqual(loader.callCount, 1)
     }
     
+    func test_pullsToRefresh_loadData() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.callCount, 2)
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.callCount, 3)
+    }
     
-    class LoaderSpy {
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://api.github.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: PullsViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = PullsViewController(loader: loader)
+        trackMemoryLeaks(instance: sut, file: file, line: line)
+        trackMemoryLeaks(instance: loader, file: file, line: line)
+        return (sut: sut, loader: loader)
+    }
+
+    
+    class LoaderSpy: PullRequestsLoader {
         private(set) var callCount: Int = 0
         
-        func load() {
+        func load(_ completion: @escaping (Result<[PullRequest], Error>) -> Void) {
             callCount += 1
         }
     }
     
+}
+
+extension XCTestCase {
+    func trackMemoryLeaks(instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
+
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
 }
